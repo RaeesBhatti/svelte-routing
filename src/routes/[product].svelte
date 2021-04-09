28f2e9e4d91017305c8c4de40d8be08258499d6a
Raees Iqbal <10067728+RaeesBhatti@users.noreply.github.com>
2021-04-09 22:16:34 +0500

    Initial commit

---
+++ src/routes/[product].svelte
@@ -0,0 +1,3 @@
+<main>
+	Product page
+</main>
---
+++ src/routes/index.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import Counter from '$lib/Counter.svelte';
+</script>
+
+<main>
+	<a href='/some-product'>/some-product</a>
+	<a href='/category/'>/category/</a>
+</main>
+
+<style>
+	a {
+			display: block;
+			margin-top: 1rem;
+			margin-bottom: 1rem;
+	}
+</style>
